1392138779f08d7fffdaff2fb6f6d7b06d3f2c87
taltongsreng <ttongsreng@gmail.com>
2024-03-29 10:24:09 +0700

    first commit

---
+++ src/main/java/gic/i4/quiz01/Quiz01Application.java
@@ -0,0 +1,13 @@
+package gic.i4.quiz01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz01Application.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/quiz01/Quiz01ApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.quiz01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
